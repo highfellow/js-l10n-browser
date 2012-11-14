b005de1a1e1cbb142232d296f3ab8afa505086c1
Andy Baxter <highfellow@gmail.com>
2012-11-14 15:09:46 +0000

    moved example files.

---
+++ example/js/example.js
@@ -0,0 +1,61 @@
+/**
+ *  Test code for node-l10n.
+ **/
+
+requirejs(['l10n','l10n-browser', 'get-string'],
+  function(l10n, l10nBrowser, getString) {
+    var outputElt = null;
+
+    function appendElt(elt, type, content) {
+      // add an element to elt and fill it.
+      var newElt = document.createElement(type);
+      newElt.textContent = content;
+      elt.appendChild(newElt);
+      return(newElt);
+    }
+
+    // run through plurals using a given adjective.
+    function repeat(elt,adjKey) {
+      var adj;
+      for (var n = 0; n < 4; n++) {
+        adj=getString(adjKey, {'n':n}, 'colour');
+        appendElt(elt, 'li', getString('phrase',{'n':n, 'adj':adj}, 'brown fox phrase'));
+      }
+    }
+
+    // test in a given language.
+    function testLanguage(lang, callback) {
+      var h2 = appendElt(outputElt, 'h2', 'Language:' + lang)
+      l10n.loadResource('data.properties', lang, function() {
+        template = "\
+<h2>Template test</h2>\
+<p data-l10n-token='tale'>A tale of two <strong>small</strong> brown furry creatures</p>\
+<p>another paragraph</p>";
+        console.log(l10nBrowser.localiseHTML(template, {}));
+        var h3 = appendElt(h2, 'h3', 'Usual version');
+        var p = appendElt(h2, 'p', '');
+        var ul = appendElt(p, 'ul', '');
+        repeat(ul, 'brown');
+        h3 = appendElt(h2, 'h3', 'With parameter substitution');
+        p = appendElt(h2, 'p', '');
+        ul = appendElt(p, 'ul', '');
+        repeat(ul, 'pink');
+        callback && callback();
+      },
+      function(err) {
+        console.log('Failed to load data.properties');
+      });
+    }
+    l10n.setAdapter(l10nBrowser, {baseURL: 'locales/'});
+    l10n.setMarkFallbacks();
+
+    window.onload = function() {
+      // initialise L10n with the browser adapter.
+      // do the tests.
+      outputElt = document.getElementById('output'); 
+      testLanguage('en', function() {
+        testLanguage('de');
+      });
+    };
+  }
+);
---
+++ example/js/get-string.js
@@ -0,0 +1,10 @@
+
+/* this code split off just to check whether multiple
+ * requires of l10n.js work OK */
+
+define(function(require) {
+  var l10n = require('l10n');
+  return function(key, args, fallback) {
+    return l10n.get(key, args, fallback);
+  }
+});
